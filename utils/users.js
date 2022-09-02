7a2c719b1fbeb85c7c31230c6a41b9f8ae33d9d6
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2022-09-02 11:18:39 -0300

    add basic auth and sign methods

---
+++ utils/users.js
@@ -0,0 +1,5 @@
+//simulate a users registry.
+//The back end for this could be anything from a decentralized protocol like Ceramic, ThreadDB, or GunDB, to a centralized service or DB.
+
+export const users = {}
+
